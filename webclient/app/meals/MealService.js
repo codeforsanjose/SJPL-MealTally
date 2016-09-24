2b248bcd04a8150a3250290b712d7db2f7eb167d
mak <bmakarand@gmail.com>
2016-09-24 16:55:33 -0700

    updated field

--- webclient/app/meals/MealService.js
+++ webclient/app/meals/MealService.js
@@ -2,7 +2,7 @@
 angular.module('app.meal').service('mealService', function ($http) {
       return {       
           addMeal : function(payload){
-              var url ="/rest/item"
+              var url ="/meal"
               return $http.post(url, payload)
                 .then(function (result) {
                     return result.data;
