c8f0840b48e1f5c83021c4d3b10331152e5a6b4f
mak <bmakarand@gmail.com>
2016-09-24 15:37:30 -0700

    updated files

--- webclient/app/meals/MealCtrl.js
+++ webclient/app/meals/MealCtrl.js
@@ -2,6 +2,24 @@
 
 angular.module('app.meal').controller('MealAddController', function ($scope) {
     $scope.test 
+    $scope.ameal = {
+        "date": '',
+        "siteName": '',
+        "meal": {
+            "type": '',
+            "vendorReceived": 0,
+            "carryOver": 0,
+            "consumed": {
+                "child": 0,
+                "adult": 0,
+                "volunteer": 0
+            },
+            "damaged": 0,
+            "wasted": 0
+
+        } 
+    }
+  
     var init = function(){
         $scope.test = "Hello World This is Meal"
     }()
