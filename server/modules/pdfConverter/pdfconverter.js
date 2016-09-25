2ad44af7ab2ac457519e3405472a5402f3627cd4
Natalie Romano <brandnew03@gmail.com>
2016-09-25 10:46:25 -0700

    pdf generator and email

--- server/modules/pdfConverter/pdfconverter.js
+++ server/modules/pdfConverter/pdfconverter.js
@@ -1,16 +1,19 @@
 'use strict'
 var fs = require('fs');
 var pdf = require('html-pdf');
-var html = fs.readFileSync('./public/templates/ymcaReport.html', 'utf8');
 var options = { format: 'Letter' };
 
-var createPDF = function(pdfDate, pdfSite) {
+var createPDF = function(htmlName, pdfDate, pdfSite, pdfMeal, callback) {
+  var html = fs.readFileSync('modules/pdfConverter/' + htmlName, 'utf8');
+  var pdfName = 'reports/Week_' + pdfDate + pdfSite + '_' + pdfMeal +'.pdf';
   pdfSite.replace(/ /g, '_');
-  pdf.create(html, options).toFile('reports/Week_' + pdfDate + pdfSite +'.pdf', function(err, res) {
+  pdf.create(html, options).toFile(pdfName, function(err, res) {
     console.log('Creating pdf..');
     if (err) return console.log(err);
     console.log(res);
+    callback();
   });
+  return pdfName;
 }
 
 module.exports = createPDF;
