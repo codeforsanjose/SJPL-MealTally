3cec686aeec45dce28f20f4936e9db04fce2d509
cy19890513 <ychen012@qq.com>
2017-04-13 19:45:32 -0700

    removed android folder and files

--- android/app/src/main/java/com/togetherly/hackathon/mealtally/Extensions.kt
+++
@@ -1,8 +0,0 @@
-package com.togetherly.hackathon.mealtally
-
-import android.content.Context
-import android.widget.Toast
-
-fun Context.showToast(text: String, length: Int = Toast.LENGTH_SHORT) {
-    Toast.makeText(this, text, length).show()
-}
