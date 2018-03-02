90364b371500bdba883c5b1839b43a516e6ae817
Joseph <jrichardson@jmstudios.net>
2018-03-01 16:54:54 -0800

    started meal tally components have generic dropdown list and incrementers

---
+++ src/components/MealTallyComponent/MealTallyComponent.jsx
@@ -0,0 +1,21 @@
+
+import * as React from 'react'
+
+//require('./incrementComponent.css');
+
+
+class MealTallyComponent extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+    }
+    render() {
+        return (
+            <div className="MealTallyContainer">
+            </div>
+        )
+    }
+}
+
+export default MealTallyComponent
