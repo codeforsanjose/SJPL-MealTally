363b86ad3ac804c4b97bf027906adf027567e104
Lisa Gee <zhusufeng@gmail.com>
2018-03-03 16:49:13 -0800

    Add simple rootReducer to return state

---
+++ redux/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+// import { combineReducers } from 'redux';
+// import * from '../actions/actions';
+
+const rootReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
