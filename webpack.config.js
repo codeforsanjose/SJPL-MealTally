3db81157684e265148444f6bb4d441696cda08d0
Joseph <jrichardson@jmstudios.net>
2018-04-14 20:27:23 -0700

    more cleanup for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const webpack = require('webpack');
 const path = require('path');
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: [
